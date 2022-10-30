03404be9fc80100747496b4c6c87378aa453ac67
stefanbrb10 <35970743+stefanbrb10@users.noreply.github.com>
2022-10-30 20:59:22 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@
 using namespace std;
 
 int main(){
-    Match M1("03.11.2022", "FCSB", "Rapid", 1, 1);
+   /* Match M1("03.11.2022", "FCSB", "Rapid", 1, 1);
     M1.Score();
     cout << endl;
     Match M2("06.11.2022", "FCSB", "West Ham", 0, 3);
-    M2.Score();
+    M2.Score();*/
+    cout << "Hello world!";
     return 0;
 }
