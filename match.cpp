cdfb733a52a1814aba8e588e751f1eabf60c37dd
stefanbrb10 <35970743+stefanbrb10@users.noreply.github.com>
2022-10-30 10:56:16 +0200

    Update match.cpp

--- match.cpp
+++ match.cpp
@@ -1,59 +1,47 @@
-#ifndef MATCHES_H
-#define MATCHES_H
-
-#include <iostream>
-#include <string>
-#include <vector>
+#include "match.h"
 
 using namespace std;
 
-class Match{
-private:
-    string date;
-    string home_team, away_team;
-    vector <int> scorers;
-    string hour;
-    int goals_home, goals_away;
-    
-public:
-    Match(string objDate, string objHome, string objAway, int objHgoals, int objAgoals){
-        date = objDate;
-        home_team = objHome;
-        away_team = objAway;
-        goals_home = objHgoals;
-        goals_away = objAgoals;
+Match::Match(const Match& m){
+    date = m.date;
+    home_team = m.home_team;
+    away_team = m.away_team;
+    goals_home = m.goals_home;
+    goals_away = m.goals_away;
+}
+
+Match::Match(string objDate, string objHome, string objAway, int objHgoals, int objAgoals){
+    date = objDate;
+    home_team = objHome;
+    away_team = objAway;
+    goals_home = objHgoals;
+    goals_away = objAgoals;
+}
+
+Match&Match::operator=(const Match& other){
+    date = other.date;
+    home_team = other.home_team;
+    away_team = other.away_team;
+    goals_home = other.goals_home;
+    goals_away = other.goals_away;
+}
+
+void Match::Score(){
+    if(goals_home > goals_away){
+    /// voi adauga puncte in clasa Team pt echipa home
     }
+    else if(goals_home < goals_away){
 
-    Match(const Match& m){
-        date = m.date;
-        home_team = m.home_team;
-        away_team = m.away_team;
-        goals_home = m.goals_home;
-        goals_away = m.goals_away;
     }
+    else {
 
-    void Score(){
-        if(goals_home > goals_away){
-            /// voi adauga puncte in clasa Team pt echipa home
-        }
-        else if(goals_home < goals_away){
-
-        }
-        else {
-
-        }
     }
+}
 
-    friend ostream& operator<<(ostream& os, Match m);
-
-    ~Match(){}
-
-};
+Match::~Match(){}
 
 ostream& operator<<(ostream& os, Match m){
-    os << m.home_team << " - " << m.away_team << " " << goals_home << "-" << goals_away;
+    os << m.home_team << " - " << m.away_team << " " << m.goals_home << "-" << m.goals_away;
     return os;
 }
-#endif
-
 
