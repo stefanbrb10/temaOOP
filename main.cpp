3998bcad0d9e1e63a8714e9fffec32e016d32eee
stefanbrb10 <35970743+stefanbrb10@users.noreply.github.com>
2022-10-30 20:38:08 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include<string>
+#include "match.h"
+#include "team.h"
+#include "player.h"
+#include<vector>
 
-int main() {
-    std::cout << "Hello, world!\n";
+using namespace std;
+
+int main(){
+    Match M1("03.11.2022", "FCSB", "Rapid", 1, 1);
+    M1.Score();
+    cout << endl;
+    Match M2("06.11.2022", "FCSB", "West Ham", 0, 3);
+    M2.Score();
     return 0;
 }
