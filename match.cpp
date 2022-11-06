b0588bd358385eb06b3e7b59dca942bcee3babf4
stefanbrb10 <35970743+stefanbrb10@users.noreply.github.com>
2022-11-06 15:01:13 +0200

    Update match.cpp

--- match.cpp
+++ match.cpp
@@ -1,4 +1,5 @@
 #include "match.h"
+#include "team.h"
 
 using namespace std;
 
@@ -10,12 +11,13 @@ Match::Match(const Match& m){
     goals_away = m.goals_away;
 }
 
-Match::Match(string objDate, string objHome, string objAway, int objHgoals, int objAgoals){
+Match::Match(string objDate, Team& objHome, Team& objAway, int objHgoals, int objAgoals, vector <Player> &objScorers){
     date = objDate;
     home_team = objHome;
     away_team = objAway;
     goals_home = objHgoals;
     goals_away = objAgoals;
+    scorers = objScorers;
 }
 
 Match&Match::operator=(const Match& other){
@@ -24,22 +26,28 @@ Match&Match::operator=(const Match& other){
     away_team = other.away_team;
     goals_home = other.goals_home;
     goals_away = other.goals_away;
+    return *this;
 }
 
 void Match::Score(){
     if(goals_home > goals_away){
-    /// voi adauga puncte in clasa Team pt echipa home
-    cout << "Castiga " << home_team;
+        /// voi adauga puncte in clasa Team pt echipa home
+        cout << "Castiga " << home_team;
     }
     else if(goals_home < goals_away){
         cout << "Castiga " << away_team;
     }
     else {
-       cout <<"Egal";
+        cout <<"Egal";
+    }
+    for(int i = 0; i < goals_home + goals_away; i++) {
+        cout << " " << scorers[i] << " ";
     }
 }
 
-Match::~Match(){}
+Match::~Match(){
+
+}
 
 ostream& operator<<(ostream& os, Match m){
     os << m.home_team << " - " << m.away_team << " " << m.goals_home << "-" << m.goals_away;
