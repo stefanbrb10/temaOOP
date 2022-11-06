5b8d3da9da6f26c2af8cc4e428a8f14d3e841939
stefanbrb10 <35970743+stefanbrb10@users.noreply.github.com>
2022-11-06 15:01:32 +0200

    Update match.h

--- match.h
+++ match.h
@@ -4,23 +4,26 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "team.h"
+#include "player.h"
 
 using namespace std;
 
 class Match{
 private:
     string date;
-    string home_team, away_team;
-    vector <int> scorers;
+    Team& home_team;
+    Team& away_team;
+    vector <Player> scorers;
     string hour;
     int goals_home, goals_away;
 public:
-    Match(string objDate, string objHome, string objAway, int objHgoals, int objAgoals);
+    Match(string objDate, Team &objHome, Team &objAway, int objHgoals, int objAgoals, vector<Player> objScorers);
     Match(const Match& m);
     Match& operator=(const Match& other);
     void Score();
     friend ostream& operator<<(ostream& os, Match m);
-   ~Match();
+    ~Match();
 };
 #endif
 
